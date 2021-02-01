dbdd5f476483236a184e89ff4295bbb94147c369
ezikechris <christophermbe1009@gmail.com>
2021-02-01 12:45:07 +0100

    javascript

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.getElementById("text").innerHTML = "Welcome";
-// this automatically gets the id assigned to the element and changes the text to a new text
-
-document.getElementById("mybutton").onclick = function() {
-    document.getElementById("text").innerHTML = "Thank You";
-    // this assigns a button on the pages and changes the text on the page based on the button on that page
+document.getElementById('d1').onclick = function() {
+    document.getElementById('d1').style.display = "none";
 }
-
